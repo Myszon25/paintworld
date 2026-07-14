90f613dfcd70eea2e35cf824b2bbe7af2dbb49ae
Myszon25 <trzeciaks20@gmail.com>
2026-07-14 19:41:10 +0200

    Initialize Cesium viewer and set default access token

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-html, body, #cesiumContainer {
-    width:100%;
-    height:100%;
-    margin:0;
-    padding:0;
-    overflow:hidden;
-}
+Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiMjkyMWNhZS00YjcxLTRlZjAtYTJlMS1iYjliNjIzZjhhZDQiLCJpZCI6NDU2NDM4LCJzdWIiOiJteXN6b24iLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoibXlzem9uX2RlZmF1bHQiLCJpYXQiOjE3ODQwNTA3OTZ9.lzeY6OMxBqQzAl2XGlZpnz4ISkClyR0uPnbi8ActwEM";
+
+const viewer = new Cesium.Viewer("cesiumContainer", {
+    terrain: Cesium.Terrain.fromWorldTerrain()
+});
+
+
+viewer.camera.flyTo({
+    destination: Cesium.Cartesian3.fromDegrees(
+        21.0122,
+        52.2297,
+        500
+    )
+});
